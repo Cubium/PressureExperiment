34585160c3b489331b451f96f32ef53cb5d07951
Carson Fox <carson.fox@aggiemail.usu.edu>
2018-06-06 16:26:38 -0600

    Itsy Bitsy uses Serial1

--- Pressure_Experiment/Pressure_Experiment.ino
+++ Pressure_Experiment/Pressure_Experiment.ino
@@ -4,13 +4,13 @@
 MS5xxx sensor(&Wire);
 
 void setup() {
-  Serial.begin(9600);
+  Serial1.begin(9600);
   delay(1000);
 }
 
 void loop() {
   sensor.ReadProm();
   sensor.Readout();
-  Serial.println(sensor.GetPres());
+  Serial1.println(sensor.GetPres());
   delay(500);
 }
